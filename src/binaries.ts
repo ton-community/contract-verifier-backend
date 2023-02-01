bed211321365a3c76f5b094399381454f3a48697
Shahar Yakir <shahar.yakir@gmail.com>
2023-02-01 11:26:23 +0200

    fix

--- src/binaries.ts
+++ src/binaries.ts
@@ -1,7 +1,8 @@
-import { FuncCompilerVersion } from "./types";
+import { FuncCompilerVersion } from "@ton-community/contract-verifier-sdk";
 
 export const funcCompilers: { [key in FuncCompilerVersion]: string } = {
   "0.2.0": "resources/binaries/0.2.0",
   "0.3.0": "resources/binaries/0.3.0",
   "0.4.0": "resources/binaries/0.4.0",
+  "0.4.1": "resources/binaries/0.4.1",
 };
