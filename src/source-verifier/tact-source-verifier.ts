4f6c1f0110e6b58bd294a25c76bb312a6d0c7ee6
Shahar Yakir <shahar.yakir@gmail.com>
2023-02-28 11:04:56 +0200

    in prog

--- src/source-verifier/tact-source-verifier.ts
+++ src/source-verifier/tact-source-verifier.ts
@@ -4,41 +4,50 @@ import { promisify } from "util";
 import { exec } from "child_process";
 import { Cell } from "ton";
 import { SourceVerifier, SourceVerifyPayload, CompileResult } from "../types";
+import { run } from "@tact-lang/compiler";
+
 const execAsync = promisify(exec);
 
 export class TactSourceVerifier implements SourceVerifier {
   async verify(payload: SourceVerifyPayload): Promise<CompileResult> {
-    const tactConfig = path.join(payload.tmpDir, "tact.config.json");
-    await fs.writeFile(
-      tactConfig,
-      JSON.stringify({
-        projects: [
-          {
-            name: "proj",
-            path: path.join(payload.tmpDir, payload.sources[0].path),
-            output: "",
-          },
-        ],
-      }),
-    );
-    const res = await execAsync(`npx tact --config ${tactConfig}`);
+    console.log(payload.sources, process.cwd());
 
-    const dirContents = await fs.readdir(path.join(payload.tmpDir));
+    const files = {
+      [payload.sources[0].path]: (await fs.readFile(payload.sources[0].path)).toString("base64"),
+    };
+
+    const config = {
+      projects: [
+        {
+          name: "proj",
+          path: payload.sources[0].path,
+          output: "",
+        },
+      ],
+    };
+
+    const res = await run({ config, files });
+
+    // const res = await execAsync(`npx tact --config ${tactConfig}`);
+
+    console.log(Object.keys(files));
+
+    const dirContents = files;
     // TODO perhaps a multi-contract tact project won't work
-    const bocFilename = dirContents.filter((x) => x.endsWith(".boc"))?.[0];
-    const abiFilename = dirContents.filter((x) => x.endsWith(".abi"))?.[0];
+    const bocFilename = Object.keys(dirContents).filter((x) => x.endsWith(".boc"))?.[0];
+    const abiFilename = Object.keys(dirContents).filter((x) => x.endsWith(".abi"))?.[0];
 
     if (!bocFilename || !abiFilename) {
       return {
         compilerSettings: { tactVersion: "0.5.0" },
-        error: res.stderr,
+        error: String("Did compilation succeed: " + String(res)),
         hash: null,
         result: "unknown_error",
         sources: [],
       };
     }
 
-    const boc = await fs.readFile(path.join(payload.tmpDir, bocFilename));
+    const boc = Buffer.from(files[bocFilename], "base64");
     const hash = Cell.fromBoc(boc)[0].hash().toString("base64");
 
     return {
