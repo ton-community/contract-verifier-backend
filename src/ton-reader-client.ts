d8e458d2d956482ce9b520cf4e30809714611747
thekiba <hr@thekiba.io>
2025-07-09 01:52:52 +0400

    refactor: use const instead of var in intToIP

--- src/ton-reader-client.ts
+++ src/ton-reader-client.ts
@@ -17,10 +17,10 @@ export interface TonReaderClient {
 }
 
 function intToIP(int: number) {
-  var part1 = int & 255;
-  var part2 = (int >> 8) & 255;
-  var part3 = (int >> 16) & 255;
-  var part4 = (int >> 24) & 255;
+  const part1 = int & 255;
+  const part2 = (int >> 8) & 255;
+  const part3 = (int >> 16) & 255;
+  const part4 = (int >> 24) & 255;
 
   return part4 + "." + part3 + "." + part2 + "." + part1;
 }
